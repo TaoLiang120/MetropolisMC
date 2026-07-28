d8d0c6b279432d2ebd9ec5dc9980baccf7e86450
Tao Liang <xhtliang120@gmail.com>
2026-07-27 21:51:13 -0400

    modified

--- MMC/script/run.py
+++ MMC/script/run.py
@@ -36,6 +36,7 @@ def main():
     shutil.copy(fdata, os.path.join(DataOut_Path, "MMC0.dat"))
 
     atom_style = thissett.PyLAMMPS["atom_style"]
+    relax_lines = thissett.PyLAMMPS["relax_lines"]
     loopmax = MMCsetts["LoopMax"]
     ratio_shift = MMCsetts["ratio_shift"]
     Exclude_types = MMCsetts["Exclude_types"]
@@ -62,7 +63,7 @@ def main():
     maxdiff = [0.0] * ntypes
 
     lmp.excute_file(infile)
-    mydata.last_TE, mydata.last_types, molids = lmp.get_total_energy_types(iloop)
+    mydata.last_TE, mydata.last_types, molids = lmp.get_total_energy_types(iloop, relax_lines=relax_lines)
     mydata.natoms = len(mydata.last_types)
     eatoms = lmp.get_eatoms(iloop, mydata.natoms)
     mydata.update_EREFs(mydata.last_types, eatoms, molids=molids, Exclude_mid=Exclude_mid)
@@ -94,7 +95,7 @@ def main():
         this_types = mydata.get_this_types(id_hot1, id_2)
         lmp.scatter_this_types(this_types)
         iloop += 1
-        mydata.this_TE, mydata.this_types, molids = lmp.get_total_energy_types(iloop)
+        mydata.this_TE, mydata.this_types, molids = lmp.get_total_energy_types(iloop, relax_lines=relax_lines)
         eatoms = lmp.get_eatoms(iloop, mydata.natoms)
 
         if iloop % Nsteps4UpdateEREFs == 0:
